96badc270070454109a7a8467e2efef162c19983
Javier Godoy <javiergodoymiguez@gmail.com>
2024-08-07 19:32:35 +0200

    javi-host

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const authRoutes = require("./auth.routes");
+
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
+
+router.use('/auth', authRoutes);
 
 module.exports = router;
