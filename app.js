e932bd392ef92021c29b6a09faf3e0289b8765e4
blankuet <blancafeijoocruz@gmail.com>
2024-08-10 15:11:34 +0200

    guest routes

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.use("/api", indexRoutes);
 const hostRoutes = require("./routes/host.routes");
 app.use("/auth/host", hostRoutes);
 
+const guestRoutes = require("./routes/auth.routes");
+app.use("/auth", guestRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
