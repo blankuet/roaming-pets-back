93b5be80f90405cfbe6ddb633d945eeb350dbe4e
blankuet <blancafeijoocruz@gmail.com>
2024-08-17 16:36:45 +0200

    Guest profile, navbar, home and dashboard updated

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -130,4 +130,73 @@ router.get("/verify", isAuthenticated, (req, res, next) => {
   res.status(200).json(req.payload);
 });
 
+//Ruta para subir una imagen de perfil
+router.post("/upload", isAuthenticated, async (req, res, next) => {
+  try {
+    const { imageUrl } = req.body;
+    console.log(imageUrl);
+    const userId = req.payload._id;  // Asume que el usuario autenticado está en el payload
+    // Buscar al usuario por ID
+    const guest = await Guest.findById(userId);
+
+    if (!guest) {
+      return res.status(404).json({ message: "Guest not found." });
+    }
+
+    // Actualizar el campo profileImage con la nueva URL
+    guest.profileImage = imageUrl;
+    await guest.save();
+
+    res.json({ message: "Profile image updated successfully" });
+  } catch (error) {
+    console.error('Error updating profile image:', error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+//Ruta para borrar el usuario
+router.delete('/delete', isAuthenticated, async (req, res) => {
+  try {
+    const userId = req.payload._id; // El ID del usuario autenticado se obtiene de `req.payload`
+
+    // Buscar y eliminar el usuario en la base de datos
+    const deletedUser = await Guest.findByIdAndDelete(userId);
+
+    if (!deletedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+//ruta para editar el perfil del usuario
+router.put("/update", isAuthenticated, async (req, res, next) => {
+  console.log('we are on update')
+  try {
+    const { id, name, lastname, email } = req.body;
+    console.log(id)
+
+    const user = await Guest.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.name = name || user.name;
+    user.lastname = lastname || user.lastname;
+    user.email = email || user.email;
+
+    await user.save();
+
+    res.status(200).json({ user });
+  } catch (error) {
+    console.error("Error updating user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 module.exports = router;
